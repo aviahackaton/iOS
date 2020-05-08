5f044bfc22159ede99b66d3db310f5667c7fb50e
Кирилл Кириленко <k.kirilenko@lucky.online>
2020-05-08 18:56:36 +0300

    Initial Commit

---
+++ aviahackaton2.0/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  aviahackaton2.0
+//
+//  Created by godemodegame on 08.05.2020.
+//  Copyright © 2020 godemodegame. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
